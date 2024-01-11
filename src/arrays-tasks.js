550831809170a6935165dfde9a2b3ff91bf8b45f
Vihnovskaya Vika <vihnovskaya1932@gmail.com>
2024-01-11 21:41:00 +0300

    feat: solve task to return an index of the element

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -56,8 +56,11 @@ function sumArrays(/* arr1, arr2 */) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  if (arr.includes(value)) {
+    return arr.indexOf(value);
+  }
+  return -1;
 }
 
 /**
