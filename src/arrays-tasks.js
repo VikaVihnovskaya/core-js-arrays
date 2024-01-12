efe8b43b4cc2cb6b7d454e72fa073768175a49b5
Vihnovskaya Vika <vihnovskaya1932@gmail.com>
2024-01-12 11:57:44 +0300

    feat: solve task to check if elements in tte array

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -147,8 +147,8 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  return arr.every((elem) => elem.length === arr[0].length);
 }
 
 /**
@@ -162,8 +162,8 @@ function isSameLength(/* arr */) {
  *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
  *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
  */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex( arr) {
+  return arr.some((elem, index) => elem === index);
 }
 
 /**
